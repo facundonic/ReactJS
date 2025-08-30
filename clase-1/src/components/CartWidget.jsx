3f2ee46f708f347375752c684c8775300239fcf9
Facundo <facugandini@hotmail.com>
2025-08-30 11:46:36 -0300

    Entrega 01

---
+++ clase-1/src/components/CartWidget.jsx
@@ -0,0 +1,15 @@
+import "../styles/CartWidget.css";
+
+const CartWidget = () => {
+  return (
+    <>
+      <a className="bx bx-cart-alt">
+        <div className="circle">
+          <div className="number">1</div>
+        </div>
+      </a>
+    </>
+  );
+};
+
+export default CartWidget;
---
+++ clase-1/src/components/ItemListContainer.jsx
@@ -0,0 +1,5 @@
+const ItemListContainer = (props) => {
+  return <h1>{props.texto}</h1>;
+};
+
+export default ItemListContainer;
